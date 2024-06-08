1c3a5800d226ff10b7ec4f7c4d81f6fd266a31ac
Dono34 <donokassing@gmail.com>
2024-06-08 11:20:06 -0400

    Added Features

--- Tutorial/jssample.js
+++ Tutorial/jssample.js
@@ -1,6 +1,7 @@
 console.log("---------------------")
 console.log("---------------------")
-console.log("Rise & Shine")
 console.log("Ready for a new Day!!")
+console.log("Test line 1")
+console.log("Test Line 2")
 console.log("---------------------")
 
